e5d490c7deadde09a28445b5ad81817860931f45
Thiago Oliveira <thiagooliv_@outlook.com>
2021-11-07 16:49:44 -0300

    feat: import Trending movies component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import BaseContainer from "./components/base/Container";
 import NewDisney from "./components/NewDisney";
 import Recommended from "./components/Recommend";
+import Trending from "./components/Trending";
 
 export default function Root() {
   return (
     <BaseContainer>
       <NewDisney />
       <Recommended />
+      <Trending />
     </BaseContainer>
   );
 }
--- src/services/listMovies.ts
+++ src/services/listMovies.ts
@@ -2,3 +2,4 @@ import movies from "./movies.json";
 
 export const newDisney = movies.newDisney;
 export const recommended = movies.recommend;
+export const trending = movies.trending;
