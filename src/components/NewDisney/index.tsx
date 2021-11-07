8383a72bd3c6a1515c28659a7f148005f23a16a0
Thiago Oliveira <thiagooliv_@outlook.com>
2021-11-07 11:57:25 -0300

    feat: new NewDisney component

---
+++ src/components/NewDisney/index.tsx
@@ -0,0 +1,20 @@
+import "./styles.scss";
+import { newDisney } from "../../services/listMovies";
+
+const NewDisney = () => {
+  return (
+    <div className="collection-new-disney">
+      <h4>New to Disney+</h4>
+      <div className="collection-new-disney__content">
+        {newDisney &&
+          newDisney.map((movie, key) => (
+            <div className="collection-new-disney__wrap" key={key}>
+              <img src={movie.cardImg} alt={movie.title} />
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default NewDisney;
