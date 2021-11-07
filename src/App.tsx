8769a1a9cc12cabbdc8f42bc6cdf4ca377bd2637
Thiago Oliveira <thiagooliv_@outlook.com>
2021-11-07 16:53:05 -0300

    feat: import Original movies component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import BaseContainer from "./components/base/Container";
 import NewDisney from "./components/NewDisney";
 import Recommended from "./components/Recommend";
 import Trending from "./components/Trending";
+import Original from "./components/Original";
 
 export default function Root() {
   return (
@@ -9,6 +10,7 @@ export default function Root() {
       <NewDisney />
       <Recommended />
       <Trending />
+      <Original />
     </BaseContainer>
   );
 }
--- src/services/listMovies.ts
+++ src/services/listMovies.ts
@@ -3,3 +3,4 @@ import movies from "./movies.json";
 export const newDisney = movies.newDisney;
 export const recommended = movies.recommend;
 export const trending = movies.trending;
+export const original = movies.original;
