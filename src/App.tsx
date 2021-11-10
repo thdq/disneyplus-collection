df5f631e3f5f248e8a3a59c893534d0e45698d19
Thiago Oliveira <thiagooliv_@outlook.com>
2021-11-10 20:35:21 -0300

    feat: new event linstener to focus on collection

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,18 @@ import NewDisney from "./components/NewDisney";
 import Recommended from "./components/Recommend";
 import Trending from "./components/Trending";
 import Original from "./components/Original";
+import { useEffect } from "react";
 
 export default function Root() {
+  useEffect(() => {
+    window.addEventListener(
+      "@disneyplus/event/focus-on-collection",
+      ({ detail }: CustomEventInit) => {
+        document.querySelector(`.collection-${detail.name}`).scrollIntoView();
+      }
+    );
+  }, []);
+
   return (
     <BaseContainer>
       <NewDisney />
